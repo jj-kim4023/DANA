3b5a65eb6d3d67d42ace5250b8d8f49fe9ad896c
jj-kim4023 <jj.kim4023@gmail.com>
2024-07-14 16:23:05 +0900

    [chore] DANA 프로젝트 초기화.

---
+++ src/test/java/com/example/dana/DanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
